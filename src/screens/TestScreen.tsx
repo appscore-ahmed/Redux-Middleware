08f660af5a76cc76556b2e88be962e239984a305
imasdurrani <demha.ahmed@gmail.com>
2020-09-03 11:46:26 +1000

    snapshot working

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Button, View, StyleSheet, FlatList } from 'react-native';
 
-export const fetchDataFromApi = (/* setResult: (result: string) => void */) => {
+export const fetchDataFromApi = (setResult: (result: string) => void) => {
   var page = 1;
   fetch(`https://randomuser.me/api/?results=10&page=${page}`)
     .then((response) => response.json())
     .then((responseJson: string) => {
-      /* setResult(responseJson); */
-      return responseJson;
+      setResult(responseJson);
+      /* return responseJson; */
     })
     .catch((error) => {
       console.log('Error selecting random data: ' + error);
